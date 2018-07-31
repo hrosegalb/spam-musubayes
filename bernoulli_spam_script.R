a2ea399df59169f940d262411d3cd38ebe69026e
Hannah Galbraith <hannahrosegalbraith@gmail.com>
2018-07-30 17:27:17 -0700

    Finished writing code to divide training/testing sets

--- bernoulli_spam_script.R
+++ bernoulli_spam_script.R
@@ -9,6 +9,12 @@ spambase <- spambase[sample(nrow(spambase)), ]
 bernoulli_spambase <- apply(spambase[1:54], MARGIN = 1:2, FUN = convert_values)
 bernoulli_spambase <- cbind(bernoulli_spambase, spambase[ , 58])
 
+num_rows <- as.integer(nrow(bernoulli_spambase))
+num_training <- as.integer(PERCENT_TRAINING * num_rows)
+
+training_set <- bernoulli_spambase[1:num_training, ]
+test_set <- bernoulli_spambase[(num_training + 1):num_rows, ]
+
 convert_values <- function(x)
 {
   if (x > 0.0)
@@ -16,4 +22,14 @@ convert_values <- function(x)
     x = 1
   }
   return(x)
+}
+
+get_feature_probabilities <- function(dataset)
+{
+  # conditional_probabilities[1, j] = P(x = 0 | 0)
+  # conditional_probabilities[2, j] = P(x = 1 | 0)
+  # conditional_probabilities[3, j] = P(x = 0 | 1)
+  # conditional_probabilities[4, j] = P(x = 1 | 1)
+  
+  
 }
