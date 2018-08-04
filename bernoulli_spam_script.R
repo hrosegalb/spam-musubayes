6be6dc746d4a074777e861e39a9a0af8ebe11cb5
Hannah Galbraith <hannahrosegalbraith@gmail.com>
2018-08-04 15:21:10 -0700

    Added function to get conditional probabilities of each feature in training set

--- bernoulli_spam_script.R
+++ bernoulli_spam_script.R
@@ -26,10 +26,74 @@ convert_values <- function(x)
 
 get_feature_probabilities <- function(dataset)
 {
-  # conditional_probabilities[1, j] = P(x = 0 | 0)
-  # conditional_probabilities[2, j] = P(x = 1 | 0)
-  # conditional_probabilities[3, j] = P(x = 0 | 1)
-  # conditional_probabilities[4, j] = P(x = 1 | 1)
+  # mean_matrix[1, j] = P(x_j = 0 | 0) => P(x_j = 0 | ham)
+  # mean_matrix[2, j] = P(x_j = 1 | 0) => P(x_j = 1 | ham)
+  # mean_matrix[3, j] = P(x_j = 0 | 1) => P(x_j = 0 | spam)
+  # mean_matrix[4, j] = P(x_j = 1 | 1) => P(x_j = 1 | spam)
   
+  num_total_rows <- nrow(dataset)
+  num_total_cols <- ncol(dataset)
+  mean_matrix <- matrix(0, nrow = 4, ncol = 54)
   
+  num_spam <- length(which(dataset[, num_total_cols] == 1))
+  num_spam <- num_spam + 2                                # Adding 2 to sum_spam as part of Laplace smoothing
+  
+  num_non_spam <- length(which(dataset[, num_total_cols] == 0))
+  num_non_spam <- num_non_spam + 2                        # Adding 2 to sum_non_spam as part of Laplace smoothing
+  
+  for (j in 1:(num_total_cols - 1))
+  {
+    sum_ones_given_ham <- 0
+    sum_zeros_given_ham <- 0
+    sum_ones_given_spam <- 0
+    sum_zeros_given_spam <- 0
+    for (i in 1:num_total_rows)
+    {
+      feature <- dataset[i, j]
+      if (dataset[i, num_total_cols] == 1)
+      {
+        if (feature == 1)
+        {
+          sum_ones_given_spam <- sum_ones_given_spam + 1
+        }
+        else
+        {
+          sum_zeros_given_spam <- sum_zeros_given_spam + 1
+        }
+      }
+      else
+      {
+        if (feature == 1)
+        {
+          sum_ones_given_ham <- sum_ones_given_ham + 1
+        }
+        else
+        {
+          sum_zeros_given_ham <- sum_zeros_given_ham + 1
+        }
+      }
+      
+      if (i == num_total_rows)
+      {
+        # Adding 1 to each sum as part of Laplace smoothing
+        sum_ones_given_spam <- sum_ones_given_spam + 1
+        sum_zeros_given_spam <- sum_zeros_given_spam + 1
+        sum_ones_given_ham <- sum_ones_given_ham + 1
+        sum_zeros_given_ham <- sum_zeros_given_ham + 1
+        
+        ones_given_spam_avg <- sum_ones_given_spam / num_spam
+        zeros_given_spam_avg <- sum_zeros_given_spam / num_spam
+        ones_given_ham_avg <- sum_ones_given_ham / num_non_spam
+        zeros_given_ham_avg <- sum_zeros_given_ham / num_non_spam
+        
+        mean_matrix[1, j] <- zeros_given_ham_avg
+        mean_matrix[2, j] <- ones_given_ham_avg
+        mean_matrix[3, j] <- zeros_given_spam_avg
+        mean_matrix[4, j] <- ones_given_spam_avg
+      }
+    }
+  }
+  return(mean_matrix)
 }
+
+mean_matrix <- get_feature_probabilities(training_set)
